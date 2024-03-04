f1942d4b468938f8653e591c3a8e975e0904e10c
Rob Foster <rjfoster1@sheffield.ac.uk>
2024-03-04 10:37:50 +0000

    Better pip install

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages, find_namespace_packages
 setup(
     name='gimmedatwave',
     version='0.1',
-    packages=find_namespace_packages(include=['gimmedatwave.*']),
+    packages=['gimmedatwave'],
 )
